9dabfa3e198c7a3c3d3444af6b421c09cf3c08d3
Malaika Saeed <smalaika902@gmail.com>
2025-07-11 21:28:38 +0500

    utitlities changes

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          utilities: './src/Utilities',
+          screens: './src/screens',
+          navigation: './src/navigation',
+          components: './src/components',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
